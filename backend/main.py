4c99c9fb9bdc33f23ee0cd078642f5525f6d80e0
Neel <neelsatasia1221@tamu.edu>
2025-11-27 14:21:29 -0600

    Cleaner code with routers

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from routers import users
+from routers import wordbank
 
 app = FastAPI()
 
 origins = [
     "http://localhost:5173"
 ]
-
-class Object(BaseModel):
-    name: str
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,16 +18,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-objects = ['obj1', "obj2"]
-
-@app.get("/objects", response_model=list)
-def read_objects():
-    return objects
-
-@app.post("/new_object", response_model=str)
-def add_person(obj: Object):
-    objects.append(obj)
-    return obj.name + " is added!"
+#routers
+app.include_router(users.router)
+app.include_router(wordbank.router)
 
 
 if __name__ == "__main__":
