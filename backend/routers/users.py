2e30b06a58aa65f6cff3db19187ec35ea4e436f1
Neel <neelsatasia1221@tamu.edu>
2025-11-28 18:59:06 -0600

    Modify routers to get and add new users

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,14 +1,41 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from database import supabase
+from models import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/")
 def get_all_users():
-    return supabase.table("test").select("*").execute().data
+    try:
+        result = supabase.table("users").select("*").execute()
+
+        if result:
+            return result.data
+        
+        return {
+            "error": "Failed to fetch users!"
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.post("/new")
-def add_user():
-    return supabase.table("test").insert({
-        "name": "Neel"
-    }).execute().data
+def add_user(new_user: User):
+    try:
+        result = supabase.table("users").insert({
+            "user_name": new_user.user_name,
+            "user_email": new_user.user_email
+        }).execute()
+
+        if result:
+            return {
+                "message": f"New user added"
+            }
+        
+        return {
+            "error": "Failed to add a new user!"
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
