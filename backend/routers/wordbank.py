4c8e5155037b3dc42d84e5ce021144f37dd0db97
Neel <neelsatasia1221@tamu.edu>
2025-12-17 14:46:38 -0600

    Change PUT's JSON input for updating categories

--- backend/routers/wordbank.py
+++ backend/routers/wordbank.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, HTTPException, Request
 from database import supabase
-from models import WordPhrase
 from typing import Dict, List
 
 router = APIRouter(prefix="/wordbank", tags=["Word Bank"])
@@ -74,25 +73,8 @@ async def new_user_word(new_data: Dict[int, List[str]], request: Request):
 
                         inserted_data.append(resp_data.data[0])
 
-                        '''
-                        if inserted_data[word_category_id] not in inserted_data:
-                            inserted_data[word_category_id] = {}
-
-                        inserted_data[word_category_id][resp_data.data[0]["word_id"]] = resp_data.data[0]["word_phrase"]
-                        '''
 
             return inserted_data
-            '''
-            if result:
-                return {
-                    "message": f"New word added for user with id {user["user_id"]}",
-                    "new_data" : result.data[0]
-                }
-
-            return {
-                "error": f"Failed to add a new word/phrase for user id {user["user_id"]}"
-            }
-            '''
         
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
@@ -120,16 +102,6 @@ async def new_user_word_cateogory(new_word_categories: List[str], request: Reque
                     inserted_data.append(resp.data[0])
 
             return inserted_data
-            '''
-            if result:
-                return {
-                    "message": f"New word category added for user with id {user["user_id"]}"
-                }
-
-            return {
-                "error": f"Failed to add a new category for user id {user["user_id"]}!"
-            }
-            '''
         
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
@@ -152,17 +124,6 @@ async def edit_word_phrase(modified_data: Dict[int, Dict[int, str]], request: Re
                         "word_phrase": word_phrase,
                         "word_category_id": word_category_id
                     }).eq("word_id", word_id).eq("user_id", user["user_id"]).execute()
-            '''
-            if result:
-                return {
-                    "message": f"A word-phrase updated for user with id {user["user_id"]}"
-                }
-            
-
-            return {
-                "error": f"Failed to update a word-phrase for user id {user["user_id"]}!"
-            }
-            '''
 
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
@@ -172,24 +133,16 @@ async def edit_word_phrase(modified_data: Dict[int, Dict[int, str]], request: Re
     }
     
 
-@router.put("/category/{word_category_id}")
-async def edit_word_category(word_category_id: int, word_category: str, request: Request):
+@router.put("/categories")
+async def edit_word_category(modified_data: Dict[int, str], request: Request):
     user = request.session.get('user')
     
     if user:
         try:
-            result = supabase.table("word_category").update({
-                "word_category": word_category
-            }).eq("word_category_id", word_category_id).eq("user_id", user["user_id"]).execute()
-
-            if result:
-                return {
-                    "message": f"A word-category updated for user with id {user["user_id"]}"
-                }
-
-            return {
-                "error": f"Failed to update a word-category for user id {user["user_id"]}!"
-            }
+            for word_category_id, new_category_name in modified_data.items():
+                supabase.table("word_category").update({
+                    "word_category": new_category_name
+                }).eq("word_category_id", word_category_id).eq("user_id", user["user_id"]).execute()
 
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
@@ -200,7 +153,7 @@ async def edit_word_category(word_category_id: int, word_category: str, request:
     
 # DELETE ---------------------------------------------------------------------------------------------------------------------------------------
 
-@router.delete("word-phrases/")
+@router.delete("/word-phrases")
 async def del_word_phrase(delete_data: List[int], request: Request):
     user = request.session.get('user')
     
@@ -208,17 +161,6 @@ async def del_word_phrase(delete_data: List[int], request: Request):
         try:
             for word_phrase_id in delete_data:
                 supabase.table("word_bank").delete().eq("word_id", word_phrase_id).eq("user_id", user["user_id"]).execute()
-
-            '''
-            if result:
-                return {
-                    "message": f"A word-phrase deleted"
-                }
-
-            return {
-                "error": f"Failed to delete a word-phrase!"
-            }
-            '''
         
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
@@ -236,17 +178,6 @@ async def del_word_category(word_category_ids: List[int], request: Request):
         try:
             for word_category_id in word_category_ids:
                 supabase.table("word_category").delete().eq("word_category_id", word_category_id).eq("user_id", user["user_id"]).execute()
-
-            '''
-            if result:
-                return {
-                    "message": f"A word-category deleted"
-                }
-
-            return {
-                "error": f"Failed to delete a word-category!"
-            }
-            '''
         
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
