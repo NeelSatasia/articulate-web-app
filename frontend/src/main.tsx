7366340131ecfc3c5eabe3569ede4050888064c0
Neel <neelsatasia1221@tamu.edu>
2025-12-19 12:33:05 -0600

    Use router to switch between webpages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import WordBank from './components/WordBank.tsx'
+
+const router = createBrowserRouter([
+    {path:"/", element: <App/>},
+    {path:"/wordbank", element: <WordBank/>}
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
