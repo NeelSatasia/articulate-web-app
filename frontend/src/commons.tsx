8c0945c4acba4ecb744859423b1f7948bb1fe83e
Neel <neelsatasia1221@tamu.edu>
2025-12-22 14:02:52 -0600

    Delete common global function exitIfNotSignedIn

--- frontend/src/commons.tsx
+++ frontend/src/commons.tsx
@@ -1,5 +1,3 @@
-import { Navigate } from "react-router-dom"
-
 export const isAuth = "isAuth"
 export const trueStr = "true"
 export const falseStr = "false"
@@ -13,11 +11,5 @@ export const initAuthInLocalStorage = () => {
 export const setAuthInLocalStorage = (error: any) => {
     if (error.response?.status === 401) {
         localStorage.setItem(isAuth, falseStr)
-    }
-}
-
-export const exitIfSignedOut = () => {
-    if (localStorage.getItem(isAuth) === falseStr) {
-        return <Navigate to="/" replace />
     }
 }
