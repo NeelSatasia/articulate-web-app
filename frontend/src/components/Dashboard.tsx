2857b97a1d619212bb1058692bd0cf407f1a9986
Neel <neelsatasia1221@tamu.edu>
2025-12-24 19:07:20 -0600

    Delete unnecessary use of user logout functionality

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -2,7 +2,7 @@ import { Navigate, useNavigate } from "react-router-dom"
 import { Button } from "./ui/button"
 import { useEffect, useState } from "react"
 import api from "../api"
-import {falseStr, initAuthInLocalStorage, isAuth, loadingStr, setAuthInLocalStorage, trueStr } from "../commons"
+import {falseStr, initAuthInLocalStorage, isAuth, loadingStr, setAuthInLocalStorage, trueStr, userName } from "../commons"
 import Loading from "./Loading"
 
 const Dashboard = () => {
@@ -25,9 +25,10 @@ const Dashboard = () => {
         const getAuth = async () => {
             if (localStorage.getItem(isAuth) === falseStr){
                 try {
-                    await api.get('/auth/me')
+                    const resp = await api.get('/auth/me')
 
                     localStorage.setItem(isAuth, trueStr)
+                    localStorage.setItem(userName, resp.data["name"])
 
                 } catch (error: any) {
                     setAuthInLocalStorage(error)
@@ -49,22 +50,11 @@ const Dashboard = () => {
         return <Navigate to={"/"} replace/>
     }
 
-    const logoutUser = async () => {
-        try {
-            localStorage.setItem(isAuth, falseStr)
-            window.location.href = "http://localhost:8000/auth/logout"
-            
-        } catch(error) {
-            console.error("Error logging user out", error)
-        }
-    }
-
     return (
         <div className="flex flex-col justify-center items-center gap-y-5">
             <h1>Dashboard</h1>
             <Button key="user-word-bank-btn" onClick={goToWordBank}>Your Word-Bank</Button>
             <Button key="learn-vocab-btn" onClick={goToVocabulary}>Learn Vocabulary</Button>
-            <Button key="logout-btn" className="bg-red-700 hover:bg-red-400" onClick={logoutUser}>Logout</Button>
         </div>
     )
 }
