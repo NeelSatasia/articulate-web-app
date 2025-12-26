6cc8047832207590c6e281b05a11e0e91f334732
Neel <neelsatasia1221@tamu.edu>
2025-12-26 12:24:44 -0600

    Display at least 5 new word-phrases every hour

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,41 +1,40 @@
-import { Navigate, useNavigate } from "react-router-dom"
-import { Button } from "./ui/button"
+import { Navigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import api from "../api"
-import {falseStr, initAuthInLocalStorage, isAuth, loadingStr, setAuthInLocalStorage, trueStr, userName } from "../commons"
+import {falseStr, initAuthInLocalStorage, isAuth, loadingStr, setAuthInLocalStorage, trueStr, userName, type WordPhrase } from "../commons"
 import Loading from "./Loading"
+import { Table, TableBody, TableCell, TableRow } from "./ui/table"
 
 const Dashboard = () => {
 
     const [loading, setLoading] = useState<boolean>(true)
 
-    const navigate = useNavigate()
-
-    const goToWordBank = () => {
-        navigate("/wordbank")
-    }
-
-    const goToVocabulary = () => {
-        navigate("/vocabulary")
-    }
-
     initAuthInLocalStorage()
+
+    const [wordPhrases, setWordPhrases] = useState<Map<number, string>>(new Map())
 
     useEffect(() => {
         const getAuth = async () => {
-            if (localStorage.getItem(isAuth) === falseStr){
-                try {
-                    const resp = await api.get('/auth/me')
+            try {
+                const resp = await api.get("/auth/me")
 
-                    localStorage.setItem(isAuth, trueStr)
-                    localStorage.setItem(userName, resp.data["name"])
+                localStorage.setItem(isAuth, trueStr)
+                localStorage.setItem(userName, resp.data["name"])
+                
+                const resp2 = await api.get("/wordbank/dashboard")
 
-                } catch (error: any) {
-                    setAuthInLocalStorage(error)
-                    console.error("Error checking authentication", error)
-                } finally {
-                    setLoading(false)
-                }
+                const tempData = new Map<number, string>()
+
+                resp2.data.forEach((row: WordPhrase) => {
+                    tempData.set(row.word_id, row.word_phrase)
+                })
+            
+                setWordPhrases(tempData)
+            } catch (error: any) {
+                setAuthInLocalStorage(error)
+                console.error("Error checking authentication", error)
+            } finally {
+                setLoading(false)
             }
         }
 
@@ -52,9 +51,19 @@ const Dashboard = () => {
 
     return (
         <div className="flex flex-col justify-center items-center gap-y-5">
-            <h1>Dashboard</h1>
-            <Button key="user-word-bank-btn" onClick={goToWordBank}>Your Word-Bank</Button>
-            <Button key="learn-vocab-btn" onClick={goToVocabulary}>Learn Vocabulary</Button>
+            <div className="w-full rounded bg-neutral-100 p-2">
+                <Table key="dashboard-word-phrases">
+                    <TableBody key="word-phrases-content">
+                        {Array.from(wordPhrases).map(([wordID, wordPhrase]) => (
+                            <TableRow key={"dashboard-word-phrase-row-" + wordID}>
+                                <TableCell key={"dashboard-word-phrase-" + wordID} className="rounded hover:bg-primary hover:text-secondary">
+                                    {wordPhrase}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     )
 }
