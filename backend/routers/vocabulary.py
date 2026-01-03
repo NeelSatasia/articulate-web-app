b503ec1c183bc00dacb0decf5bd8dc379a9f2f60
Neel <neelsatasia1221@tamu.edu>
2026-01-02 23:25:25 -0600

    WIP: Use userclient for database calls

--- backend/routers/vocabulary.py
+++ backend/routers/vocabulary.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, HTTPException, Request
+from userclient import get_user_client
+from fastapi import APIRouter, HTTPException, Request, Depends
 from database import supabase
-from typing import Dict, List
 from datetime import datetime, timezone, timedelta
 from fastapi.concurrency import run_in_threadpool
 
 router = APIRouter(prefix="/vocabulary", tags=["Vocabulary"])
+
+# GET ---------------------------------------------------------------------------------------------------------------------------------------
 
 @router.get("")
 async def user_vocabulary(request: Request):
@@ -26,15 +28,16 @@ async def user_vocabulary(request: Request):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
+    
 @router.get("/dashboard")
-async def user_dashboard(request: Request):
-    user = request.session.get('user')
+async def user_dashboard(supabase=Depends(get_user_client)):
+    #user = request.session.get('user')
 
-    if not user:
-        raise HTTPException(status_code=401, detail="Not authenticated")
+    #if not user:
+        #raise HTTPException(status_code=401, detail="Not authenticated")
 
     try:
-        result = await run_in_threadpool(lambda: supabase.table("user_vocabulary").select("vocab_word_id, word_id, vocabulary_words(word, definition), created_at").eq("user_id", user["user_id"]).order("created_at", desc=True).limit(3).execute())
+        result = await run_in_threadpool(lambda: supabase.table("user_vocabulary").select("vocab_word_id, word_id, vocabulary_words(word, definition), created_at").order("created_at", desc=True).execute())
 
         if result:
             if len(result.data) > 0 and datetime.now(timezone.utc) - datetime.fromisoformat(result.data[0]["created_at"].replace("Z", "+00:00")) < timedelta(hours=24):
@@ -45,18 +48,19 @@ async def user_dashboard(request: Request):
                         "word": row["vocabulary_words"]["word"],
                         "definition": row["vocabulary_words"]["definition"]
                     }
-                    
+
                     for row in result.data
                 ]
+
                 return flattened
             else:
-                new_result = await run_in_threadpool(lambda: supabase.rpc('get_unique_vocabulary_words_for_user', { "p_user_id": user["user_id"], "limit_count": 3 }).execute())
+                new_result = await run_in_threadpool(lambda: supabase.rpc('get_unique_vocabulary_words_for_user', { "limit_count": 3 }).execute())
 
                 if new_result:
                     return new_result.data
 
         return {
-            "error": f"Failed to fetch current vocabulary words for user with id {user['user_id']}!"
+            "error": f"Failed to fetch current vocabulary words for user!"
         }
 
     except Exception as e:
