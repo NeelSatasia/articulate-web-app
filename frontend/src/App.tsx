be923f19323a06185a1dbf6760e11d9848348081
Neel <neelsatasia1221@tamu.edu>
2026-01-02 23:12:15 -0600

    Google sign in redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "./components/ui/button"
 
 function App() {
     const googleSign = () => {
-        window.location.href = "http://localhost:8000/auth/login"
+        window.location.href =  "http://localhost:8000/auth/login"
     }
 
     return (
