26a2018463bb895daf80712f111b5df7478fde04
Neel <neelsatasia1221@tamu.edu>
2026-01-03 22:19:49 -0600

    Store/remove vocabulary for user

--- backend/routers/vocabulary.py
+++ backend/routers/vocabulary.py
@@ -1,28 +1,34 @@
 from userclient import get_user_client
 from fastapi import APIRouter, HTTPException, Request, Depends
-from database import supabase
-from datetime import datetime, timezone, timedelta
 from fastapi.concurrency import run_in_threadpool
+from models import VocabularyWordInfo
 
 router = APIRouter(prefix="/vocabulary", tags=["Vocabulary"])
 
 # GET ---------------------------------------------------------------------------------------------------------------------------------------
 
 @router.get("")
-async def user_vocabulary(request: Request):
-    user = request.session.get('user')
-
-    if not user:
-        raise HTTPException(status_code=401, detail="Not authenticated")
+async def user_vocabulary(supabase=Depends(get_user_client)):
     
     try:
-        result = await run_in_threadpool(lambda: supabase.table("user_vocabulary").select("vocab_word_id, vocabulary_words(word, definition)").eq("user_id", user["user_id"]).order("vocabulary_words.word_id").execute())
+        result = await run_in_threadpool(lambda: supabase.table("user_vocabulary").select("word_id, vocabulary_words(word, definition, frequency_score)").order("word_id").execute())
 
         if result:
-            return result.data
+
+            cleaned_data = []
+
+            for item in result.data:
+                cleaned_data.append({
+                    "word_id": item["word_id"], 
+                    "word": item["vocabulary_words"]["word"], 
+                    "definition": item["vocabulary_words"]["definition"], 
+                    "frequency_score": item["vocabulary_words"]["frequency_score"]
+                })
+
+            return cleaned_data
         
         return {
-            "error": f"Failed to fetch vocabulary for user with id {user['user_id']}!"
+            "error": f"Failed to fetch vocabulary for user!"
         }
     
     except Exception as e:
@@ -31,37 +37,64 @@ async def user_vocabulary(request: Request):
     
 @router.get("/dashboard")
 async def user_dashboard(supabase=Depends(get_user_client)):
-    #user = request.session.get('user')
-
-    #if not user:
-        #raise HTTPException(status_code=401, detail="Not authenticated")
 
     try:
-        result = await run_in_threadpool(lambda: supabase.table("user_vocabulary").select("vocab_word_id, word_id, vocabulary_words(word, definition), created_at").order("created_at", desc=True).execute())
+        
+        new_result = await run_in_threadpool(lambda: supabase.rpc('get_unique_vocabulary_words_for_user').execute())
 
-        if result:
-            if len(result.data) > 0 and datetime.now(timezone.utc) - datetime.fromisoformat(result.data[0]["created_at"].replace("Z", "+00:00")) < timedelta(hours=24):
-                flattened = [
-                    {
-                        "vocab_word_id": row["vocab_word_id"],
-                        "word_id": row["word_id"],
-                        "word": row["vocabulary_words"]["word"],
-                        "definition": row["vocabulary_words"]["definition"]
-                    }
+        if new_result:
+            return new_result.data
 
-                    for row in result.data
-                ]
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
 
-                return flattened
-            else:
-                new_result = await run_in_threadpool(lambda: supabase.rpc('get_unique_vocabulary_words_for_user', { "limit_count": 3 }).execute())
+# POST --------------------------------------------------------------------------------------------------------------------------------------
 
-                if new_result:
-                    return new_result.data
+@router.post("")
+async def add_vocabulary_word(word_info: VocabularyWordInfo, request: Request, supabase=Depends(get_user_client)):
 
-        return {
-            "error": f"Failed to fetch current vocabulary words for user!"
+    user = request.session.get('user')
+
+    try:
+        insert_data = {
+            "user_id": user["user_id"],
+            "word_id": word_info.word_id
         }
 
+        result = await run_in_threadpool(lambda: supabase.table("user_vocabulary").insert(insert_data).execute())
+
+        if result:
+            return {
+                "message": f"Successfully added word with id {word_info.word_id} to user's vocabulary!"
+            }
+        
+        return {
+            "error": f"Failed to add word with id {word_info.word_id} to user's vocabulary!"
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+
+# DELETE ------------------------------------------------------------------------------------------------------------------------------------
+
+@router.delete("")
+async def remove_vocabulary_word(word_info: VocabularyWordInfo, request: Request, supabase=Depends(get_user_client)):
+
+    user = request.session.get('user')
+
+    try:
+        result = await run_in_threadpool(lambda: supabase.table("user_vocabulary").delete().eq("word_id", word_info.word_id).execute())
+
+        if result:
+            return {
+                "message": f"Successfully removed word with id {word_info.word_id} from user's vocabulary!"
+            }
+        
+        return {
+            "error": f"Failed to remove word with id {word_info.word_id} from user's vocabulary!"
+        }
+    
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
