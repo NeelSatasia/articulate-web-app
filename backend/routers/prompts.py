7d77e024463dbc141ba2c3a718e263924609f2b7
Neel <neelsatasia1221@tamu.edu>
2026-02-01 23:05:36 -0600

    Prompts router

---
+++ backend/routers/prompts.py
@@ -0,0 +1,23 @@
+from userclient import get_user_client
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.concurrency import run_in_threadpool
+
+router = APIRouter(prefix="/prompts", tags=["Prompts"])
+
+# GET ---------------------------------------------------------------------------------------------------------------------------------------
+
+@router.get("")
+async def user_vocabulary(supabase=Depends(get_user_client)):
+    
+    try:
+        result = await run_in_threadpool(lambda: supabase.table("prompts").select("*").execute())
+
+        if result:
+            return result.data
+        
+        return {
+            "error": f"Failed to fetch prompts for user!"
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
