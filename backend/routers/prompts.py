5499d5a3509b6b41675c91beedb9c0dc2cbef4a1
Neel <neelsatasia1221@tamu.edu>
2026-02-03 22:56:44 -0600

    Shuffle the fetched prompts

--- backend/routers/prompts.py
+++ backend/routers/prompts.py
@@ -1,18 +1,20 @@
 from userclient import get_user_client
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.concurrency import run_in_threadpool
+import random
 
 router = APIRouter(prefix="/prompts", tags=["Prompts"])
 
 # GET ---------------------------------------------------------------------------------------------------------------------------------------
 
 @router.get("")
-async def user_vocabulary(supabase=Depends(get_user_client)):
+async def prompts(supabase=Depends(get_user_client)):
     
     try:
         result = await run_in_threadpool(lambda: supabase.table("prompts").select("*").execute())
 
         if result:
+            random.shuffle(result.data)
             return result.data
         
         return {
