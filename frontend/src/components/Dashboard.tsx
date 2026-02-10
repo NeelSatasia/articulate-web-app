34f91ce375e8c1ee6a0ae86948872c5c432517e8
Neel <neelsatasia1221@tamu.edu>
2026-02-09 21:04:34 -0600

    Add daily user vocabulary content

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,17 +1,16 @@
 import { Navigate } from "react-router-dom"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import api from "../api"
-import {falseStr, isAuth, loadingStr, setAuthInLocalStorage, trueStr, userName, type VocabularyWord, type WordPhrase } from "../commons"
+import {isAuth, loadingStr, setAuthInLocalStorage, trueStr, type VocabularyWord, type WordPhrase } from "../commons"
 import Loading from "./Loading"
 import { Table, TableBody, TableCell, TableRow } from "./ui/table"
-import { Button } from "./ui/button"
 
 const Dashboard = () => {
 
     const [loading, setLoading] = useState<boolean>(true)
 
-    const [wordPhrases, setWordPhrases] = useState<Map<number, string>>(new Map())
-    const [newVocabulary, setNewVocabulary] = useState<VocabularyWord[]>([])
+    const [todaysWordPhrases, setTodaysWordPhrases] = useState<Map<number, string>>(new Map())
+    const [todaysVocabulary, setTodaysVocabulary] = useState<VocabularyWord[]>([])
 
     useEffect(() => {
         const getAuth = async () => {
@@ -27,10 +26,9 @@ const Dashboard = () => {
                 const getCurrentVocab = await api.get("/vocabulary/dashboard")
 
                 localStorage.setItem(isAuth, trueStr)
-                localStorage.setItem(userName, "User") // TODO: Replace with actual user name from backend
             
-                setWordPhrases(tempData)
-                setNewVocabulary(getCurrentVocab.data)
+                setTodaysWordPhrases(tempData)
+                setTodaysVocabulary(getCurrentVocab.data)
             } catch (error: any) {
                 setAuthInLocalStorage(error)
                 console.error("Error checking authentication", error)
@@ -46,17 +44,19 @@ const Dashboard = () => {
         return <Loading spinnerAction={loadingStr}/>
     }
 
-    if (localStorage.getItem(isAuth) === falseStr) {
-        return <Navigate to={"/"} replace/>
+    if (localStorage.getItem(isAuth) !== trueStr) {
+        return <Navigate to={"/"} replace />
     }
 
     return (
         <div className="flex flex-col justify-center items-center gap-y-5 p-4">
             <div className="w-full p-2 outline outline-offset outline-primary rounded">
-                <h1 className="text-3xl mb-2">Today's Word Phrases</h1>
+                <h1 className="text-3xl mb-2">Today's Word-Phrases</h1>
+
+                {todaysWordPhrases.size > 0 ? 
                 <Table key="dashboard-word-phrases" className="rounded bg-neutral-100">
                     <TableBody key="word-phrases-content">
-                        {Array.from(wordPhrases).map(([wordID, wordPhrase]) => (
+                        {Array.from(todaysWordPhrases).map(([wordID, wordPhrase]) => (
                             <TableRow key={"dashboard-word-phrase-row-" + wordID}>
                                 <TableCell key={"dashboard-word-phrase-" + wordID} className="rounded hover:bg-primary hover:text-secondary">
                                     {wordPhrase}
@@ -64,14 +64,15 @@ const Dashboard = () => {
                             </TableRow>
                         ))}
                     </TableBody>
-                </Table>
+                </Table> : "Your word-phrases collection is currently empty. Navigate to Commonplace Book to add your word-phrases."}
             </div>
 
             <div className="w-full p-2 outline outline-offset outline-primary rounded">
                 <h1 className="text-3xl mb-2">Today's Vocabulary</h1>
+                {todaysVocabulary.length > 0 ?
                 <Table key="dashboard-new-vocabulary" className="rounded bg-neutral-100">
                     <TableBody key="new-vocabulary-content">
-                        {newVocabulary.map(vocabulary => (
+                        {todaysVocabulary.map(vocabulary => (
                             <TableRow key={"dashboard-new-vocabulary-row-" + vocabulary.word_id} className="hover:bg-primary hover:text-secondary rounded">
                                 <TableCell key={"dashboard-new-vocabulary-" + vocabulary.word_id} className="rounded-l-md">
                                     {vocabulary.word}
@@ -82,7 +83,7 @@ const Dashboard = () => {
                             </TableRow>
                         ))}
                     </TableBody>
-                </Table>
+                </Table> : "Your vocabulary collection is currently empty. Navigate to Vocabulary to search and add new vocabulary words."}
             </div>
         </div>
     )
