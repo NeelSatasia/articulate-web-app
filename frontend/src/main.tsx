18e1764325a78eb57305d5680d623c1dff4b6cc1
Neel <neelsatasia1221@tamu.edu>
2026-02-09 21:08:42 -0600

    Add vocabulary-recall endpoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,23 +9,25 @@ import Vocabulary from './components/Vocabulary.tsx'
 import Layout from './components/Layout.tsx'
 import RewritePhrases from './components/RewritePhrases.tsx'
 import EssenceWriting from './components/EssenceWriting.tsx'
+import VocabularyRecall from './components/VocabularyRecall.tsx'
 
 const router = createBrowserRouter([
-  {path: "/", element: <App />},
-  {path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, path: "dashboard", element: <Dashboard /> },
-      { path: "wordbank", element: <WordBank /> },
-      { path: "vocabulary", element: <Vocabulary /> },
-      { path: "sentence-crafting", element: <RewritePhrases /> },
-      { path: "essence-writing", element: <EssenceWriting />}
-    ]
-  }
+    {path: "/", element: <App />},
+    {path: "/",
+        element: <Layout />,
+        children: [
+            { index: true, path: "dashboard", element: <Dashboard /> },
+            { path: "wordbank", element: <WordBank /> },
+            { path: "vocabulary", element: <Vocabulary /> },
+            { path: "sentence-crafting", element: <RewritePhrases /> },
+            { path: "vocabulary-recall", element: <VocabularyRecall />},
+            { path: "essence-writing", element: <EssenceWriting />}
+        ]
+    }
 ])
 
 createRoot(document.getElementById('root')!).render(
     
-        <RouterProvider router={router} />
+    <RouterProvider router={router} />
     
 )
