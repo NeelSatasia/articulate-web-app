fb50727e651f59a7e68924e3c6ee287a799c60ed
Neel <neelsatasia1221@tamu.edu>
2026-03-06 22:51:40 -0600

    Add Settings option

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -10,6 +9,7 @@ import Layout from './components/Layout.tsx'
 import RewritePhrases from './components/RewritePhrases.tsx'
 import EssenceWriting from './components/EssenceWriting.tsx'
 import VocabularyRecall from './components/VocabularyRecall.tsx'
+import Settings from './components/Settings.tsx'
 
 const router = createBrowserRouter([
     {path: "/", element: <App />},
@@ -20,8 +20,9 @@ const router = createBrowserRouter([
             { path: "wordbank", element: <WordBank /> },
             { path: "vocabulary", element: <Vocabulary /> },
             { path: "sentence-crafting", element: <RewritePhrases /> },
-            { path: "vocabulary-recall", element: <VocabularyRecall />},
-            { path: "essence-writing", element: <EssenceWriting />}
+            { path: "vocabulary-recall", element: <VocabularyRecall /> },
+            { path: "essence-writing", element: <EssenceWriting /> },
+            { path: "settings", element: <Settings /> }
         ]
     }
 ])
