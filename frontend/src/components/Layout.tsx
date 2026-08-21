b4df8cbdf3c387f3780c73b6f7fa1c5481e0382d
Neel <neelsatasia1221@tamu.edu>
2026-08-20 21:20:29 -0500

    Restore AppSidebar instance

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import AppSidebar from "./AppSidebar"
 
 
 export default function Layout() {
 
   return (
     <SidebarProvider>
+      <AppSidebar />
       <main className="flex-1">
         <SidebarTrigger />
         <Outlet />
