45b5c4254d70276c26f0622046d30c5b7037e496
Neel <neelsatasia1221@tamu.edu>
2026-08-20 19:01:24 -0500

    Remove some paths

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,8 @@ import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import WordBank from './components/WordBank.tsx'
 import Dashboard from './components/Dashboard.tsx'
-import Vocabulary from './components/Vocabulary.tsx'
 import Layout from './components/Layout.tsx'
-import RewritePhrases from './components/RewritePhrases.tsx'
-import EssenceWriting from './components/EssenceWriting.tsx'
-import VocabularyRecall from './components/VocabularyRecall.tsx'
 import Settings from './components/Settings.tsx'
-import FreeWriting from './components/FreeWriting.tsx'
 
 const router = createBrowserRouter([
     {path: "/", element: <App />},
@@ -19,11 +14,6 @@ const router = createBrowserRouter([
         children: [
             { index: true, path: "dashboard", element: <Dashboard /> },
             { path: "wordbank", element: <WordBank /> },
-            { path: "vocabulary", element: <Vocabulary /> },
-            { path: "sentence-crafting", element: <RewritePhrases /> },
-            { path: "vocabulary-recall", element: <VocabularyRecall /> },
-            { path: "essence-writing", element: <EssenceWriting /> },
-            { path: "free-writing", element: <FreeWriting /> }, 
             { path: "settings", element: <Settings /> }
         ]
     }
