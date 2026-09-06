74056b5723ee388127624eed528cd318024c6356
Neel <neelsatasia1221@tamu.edu>
2026-09-06 18:48:33 -0500

    Remove daily-email switch feature

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react"
-import { Label } from "./ui/label"
-import { Switch } from "./ui/switch"
 import { isAuth, setAuthInLocalStorage, trueStr } from "../commons"
 import api from "../api"
 import Loading from "./Loading"
@@ -8,7 +6,6 @@ import { Navigate } from "react-router-dom"
 
 const Settings = () => {
 
-    const [dailyRecall, setDailyRecall] = useState<boolean>(false)
     const [loading, setLoading] = useState<boolean>(false)
 
     useEffect(() => {
@@ -16,10 +13,9 @@ const Settings = () => {
             setLoading(true)
             try {
                 if (localStorage.getItem(isAuth) === trueStr) {
-                    const resp = await api.get("/user")
+                    await api.get("/auth/check")
                     
                     localStorage.setItem(isAuth, trueStr)
-                    setDailyRecall(resp.data[0].daily_recall_email)
                 }
             } catch (error) {
                 setAuthInLocalStorage(error)
@@ -32,18 +28,6 @@ const Settings = () => {
         getUserData()
     }, [])
 
-    const handleDailyRecallEmailSwitch = async (checked: boolean) => {
-
-        try {
-            await api.put(`/user/daily_recall_email/${checked}`)
-            setDailyRecall(checked)
-
-        } catch (error) {
-            setAuthInLocalStorage(error)
-            console.error("Failed to update daily recall switch", error)
-        }
-    }
-
     if (localStorage.getItem(isAuth) !== trueStr) {
         return <Navigate to={"/"} replace />
     }
@@ -54,10 +38,6 @@ const Settings = () => {
 
     return (
         <div className="p-4">
-            <div className="flex flex-row p-2 bg-neutral-100 gap-x-4 w-fit rounded">
-                <Label>Send a recall email everyday</Label>
-                <Switch checked={dailyRecall} onCheckedChange={handleDailyRecallEmailSwitch} />
-            </div>
         </div>
     )
 }
