3d7f51bf01f3f5fe31719374b8e64a79aa8a3231
Neel <satasianeel021@gmail.com>
2026-09-12 11:10:32 -0500

    Change situation_only to situation

--- backend/routers/ai.py
+++ backend/routers/ai.py
@@ -143,7 +143,7 @@ async def generate_situation(request: Request, target_word: TargetWord, supabase
             text_format=Situation
         )
 
-        await run_in_threadpool(lambda: supabase.table("users").update({"situation": response.output_parsed.situation_only, "target_word_id": target_word.word_id, "attempts": 0}).not_.is_("user_id", "null").execute())
+        await run_in_threadpool(lambda: supabase.table("users").update({"situation": response.output_parsed.situation, "target_word_id": target_word.word_id, "attempts": 0}).not_.is_("user_id", "null").execute())
 
         return response.output_parsed
 
